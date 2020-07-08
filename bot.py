052ecdfeb694e05bcba4f76103250ae5c9fc0e00
viru <virufac@protonmail.com>
2020-07-07 21:57:20 -0500

    testing bot on_message from heroku

--- bot.py
+++ bot.py
@@ -4,11 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await message.channel.send("This is a test.")
+
+
 
 client.run(TOKEN)
