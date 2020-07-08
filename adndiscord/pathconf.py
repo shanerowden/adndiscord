7eb059d32f59a128a64c471c5c804b3414c257d5
viru <virufac@protonmail.com>
2020-07-07 22:52:35 -0500

    adjusted pathconf for heroku deploy

--- adndiscord/pathconf.py
+++ adndiscord/pathconf.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 USER_HOME = Path.home()
-PROJECTS_DIR = USER_HOME / "Projects"
-PROJECT_NAME = "adndiscord"
-PROJECT_ROOT = PROJECTS_DIR / PROJECT_NAME
+# PROJECTS_DIR = USER_HOME / "Projects"
+# PROJECT_NAME = "adndiscord"
+PROJECT_ROOT = Path.cwd()
 
 PATHS = {
-    'PROJECT_ROOT': PROJECTS_DIR / PROJECT_NAME,
+    'PROJECT_ROOT': PROJECT_ROOT,
     'LOG_FILE': Path(PROJECT_ROOT / "log.txt").resolve()
 }
 
