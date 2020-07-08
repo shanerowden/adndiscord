afccfa2027e43f322126329cf928f24b75e1b1de
viru <virufac@protonmail.com>
2020-07-07 23:01:29 -0500

    adjusted pathconf for heroku deploy

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from main import init
 from adndiscord.attributes import AbilityScoreGen
+from textwrap import dedent as wrap
 
 import os
 import discord
@@ -25,14 +26,20 @@ async def on_ready():
     )
 
     testing_channel = client.get_channel(730254366740709397)
-    await testing_channel.send("Discord Bot is Online and Ready. Be aware that bot commands are only available when I am running the server. They will not work when it is off.")
+    await testing_channel.send(wrap(
+        """
+        Discord Bot is Online and Ready. 
+        Be aware that bot commands are only available when I am running the server. 
+        They will not work when it is off.
+        """
+    ))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message == "!roll attributes":
+    if message == "!roll":
         ability_scores = AbilityScoreGen()
         await message.channel.send(str(ability_scores))
 
